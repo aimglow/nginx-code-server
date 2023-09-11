e69adafd318adb2bcd5f8b61906e7d2f7c68d38c
taiga morikawa <65807636+taigamorikawa@users.noreply.github.com>
2023-09-12 01:55:36 +0900

    Update set-firewall-for-cloudflare.sh

--- set-firewall-for-cloudflare.sh
+++ set-firewall-for-cloudflare.sh
@@ -6,31 +6,33 @@ RES=$(curl --request GET \
   --header 'Content-Type: application/json' )
 STA=$(echo $RES | jq .success)
 if [ $STA == true ]; then
+    jq --version > /dev/null
+    if [ $? -eq 0 ]; then
 
-    # 既存のルール削除
-    EXIST_RULE=$(firewall-cmd --list-all | grep -E 'prefix="cloudflare"' | sed 's/^.*rule/rule/')
-    IFS=$'\n'
-    for RULE in $EXIST_RULE
-    do
-        firewall-cmd --permanent --zone=public --remove-rich-rule='"'"$RULE"'"'
-    done
-    firewall-cmd --reload
+        # 既存のルール削除
+        EXIST_RULE=$(firewall-cmd --list-all | grep -E 'prefix="cloudflare"' | sed 's/^.*rule/rule/')
+        IFS=$'\n'
+        for RULE in $EXIST_RULE
+        do
+            firewall-cmd --permanent --zone=public --remove-rich-rule='"'"$RULE"'"'
+        done
+        firewall-cmd --reload
 
-    # 新しいIPv4ルール設定
-    IPV4=$(echo $RES | jq -r .result.ipv4_cidrs[])
-    for ip in $IPV4
-    do
-        RULE="rule family=ipv4 source address=$ip port port=https protocol=tcp log prefix=cloudflare accept"
-        firewall-cmd --permanent --zone=public --add-rich-rule='"'"$RULE"'"'
-    done
+        # 新しいIPv4ルール設定
+        IPV4=$(echo $RES | jq -r .result.ipv4_cidrs.[])
+        for ip in $IPV4
+        do
+            RULE="rule family=ipv4 source address=$ip port port=https protocol=tcp log prefix=cloudflare accept"
+            firewall-cmd --permanent --zone=public --add-rich-rule='"'"$RULE"'"'
+        done
 
-    # 新しいIPv6ルール設定
-    IPV6=$(echo $RES | jq -r .result.ipv6_cidrs[])
-    for ip in $IPV6
-    do
-        RULE="rule family=ipv6 source address=$ip port port=https protocol=tcp log prefix=cloudflare accept"
-        firewall-cmd --permanent --zone=public --add-rich-rule='"'"$RULE"'"'
-    done
-    firewall-cmd --reload
-
+        # 新しいIPv6ルール設定
+        IPV6=$(echo $RES | jq -r .result.ipv6_cidrs.[])
+        for ip in $IPV6
+        do
+            RULE="rule family=ipv6 source address=$ip port port=https protocol=tcp log prefix=cloudflare accept"
+            firewall-cmd --permanent --zone=public --add-rich-rule='"'"$RULE"'"'
+        done
+        firewall-cmd --reload
+    fi
 fi
